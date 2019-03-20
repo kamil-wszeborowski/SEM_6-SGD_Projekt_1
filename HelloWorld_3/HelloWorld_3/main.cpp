6c62bcdf9033eac0b12f96d41e53d08b9f46812c
kamil-wszeborowski <s16215@pjwstk.edu.pl>
2019-03-20 20:03:54 +0100

    Stworzenie szkieletu gry

--- HelloWorld_3/HelloWorld_3/main.cpp
+++ HelloWorld_3/HelloWorld_3/main.cpp
@@ -1,3 +1,51 @@
+#include "Game.h";
+
+Game *game = nullptr;
+
+int main(int arg, char *argv[]) {
+
+	game = new Game();
+
+	game->init("PeopleWar", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, true);
+
+	while (game->running()) {
+
+		game->handleEvents();
+		game->update();
+		game->render();
+
+	}
+	game->clean();
+
+	return 0;
+
+}
+
+/*
+#include "SDL.h";
+
+int main(int arg, char *argv[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = SDL_CreateWindow("PeopleWar", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(3000);
+
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(3000);
+
+	return 0;
+}
+
+
+
 
 #include <chrono>
 #include <cstdint>
@@ -89,7 +137,7 @@ int main(int, char **) {
 }
 
 
-/*
+
 #include "stdafx.h"
 #include <iostream>
 #include <SDL.h>
